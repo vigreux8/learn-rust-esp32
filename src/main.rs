e5728b5c1113db7c083e35fc9f204a23f3ddee30
vigreux8 <vigreux8@hotmail.fr>
2026-02-16 19:23:11 +0100

    feat(wifi) : rajout du  bouton hello-world

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,26 @@ use esp_idf_svc::sys::EspError;
 
 // 1. DIS À RUST QUE LE DOSSIER  EXISTE
 mod servo;
+mod wifi;
 
 // 2. IMPORTE TES STRUCTURES DEPUIS CE MODULE
 use crate::servo::ServoBus;
+use crate::wifi::WifiServer;
 
 fn main() -> Result<(), EspError> {
+    esp_idf_svc::sys::link_patches();
+    esp_idf_svc::log::EspLogger::initialize_default();
+
     // 1. Récupération des périphériques
     let peripherals = Peripherals::take()?;
 
-    // 2. Initialisation du Bus unique (Timer 0)
+    // 2. Point d'accès Wi-Fi + serveur web embarqué
+    let _wifi_server = WifiServer::start(peripherals.modem)?;
+
+    // 3. Initialisation du Bus unique (Timer 0)
     let bus = ServoBus::new(peripherals.ledc.timer0)?;
 
-    // 3. Création des servos individuels
+    // 4. Création des servos individuels
     // On donne simplement le channel et la pin GPIO
     let mut servo_bras = bus.add_servo(peripherals.ledc.channel0, peripherals.pins.gpio2)?;
     let mut servo_pince = bus.add_servo(peripherals.ledc.channel1, peripherals.pins.gpio33)?;
