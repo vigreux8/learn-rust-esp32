dba9f9ece599849bb02d8c92788f8085cb21c524
vigreux8 <vigreux8@hotmail.fr>
2026-02-18 16:33:51 +0100

    rename(hardware) :  on peut confondre hardware et peripherals j'ai donc renommer en device pour rendre maniére explicite que se sont des composant externe a l'esp32

--- src/hardware/manager.rs
+++ src/hardware/manager.rs
@@ -4,11 +4,11 @@ use esp_idf_svc::hal::ledc::LedcTimer;
 use esp_idf_svc::hal::peripheral::Peripheral;
 use esp_idf_svc::sys::EspError;
 
-pub struct HardwareManager<'a, T: LedcTimer> {
+pub struct HardwareDevices<'a, T: LedcTimer> {
     servo_bus: ServoBus<'a, T>,
 }
 
-impl<'a, T: LedcTimer> HardwareManager<'a, T> {
+impl<'a, T: LedcTimer> HardwareDevices<'a, T> {
     pub fn new(timer_periph: impl Peripheral<P = T> + 'a) -> Result<Self, EspError> {
         let servo_bus = ServoBus::new(timer_periph)?;
         Ok(Self { servo_bus })
